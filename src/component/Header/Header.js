05ac939e70cb8aba78912bb0d6a5ea82571ade12
progchris00 <prog.christian07@gmail.com>
2025-01-25 21:02:34 +0800

    refactor: separate filter button function

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -30,36 +30,7 @@ function createDropdownSection() {
 
   container.appendChild(createDropdowns());
 
-  const applyFiltersContainer = document.createElement("div");
-  applyFiltersContainer.classList.add("self-end");
-  const applyFiltersButton = document.createElement("button");
-  applyFiltersButton.classList.add(
-    "bg-gray-700",
-    "text-white",
-    "rounded-md",
-    "py-2",
-    "px-4",
-    "font-semibold",
-    "flex",
-    "items-center",
-    "gap-2",
-  );
-  applyFiltersButton.innerHTML = `<svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="size-3"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M2.628 1.601C5.028 1.206 7.49 1 10 1s4.973.206 7.372.601a.75.75 0 0 1 .628.74v2.288a2.25 2.25 0 0 1-.659 1.59l-4.682 4.683a2.25 2.25 0 0 0-.659 1.59v3.037c0 .684-.31 1.33-.844 1.757l-1.937 1.55A.75.75 0 0 1 8 18.25v-5.757a2.25 2.25 0 0 0-.659-1.591L2.659 6.22A2.25 2.25 0 0 1 2 4.629V2.34a.75.75 0 0 1 .628-.74Z"
-                clip-rule="evenodd"
-              />
-            </svg>`;
-  applyFiltersButton.innerHTML += "Apply filters";
-  applyFiltersContainer.appendChild(applyFiltersButton);
-
-  container.appendChild(applyFiltersContainer);
+  container.appendChild(createFilterButton());
 
   return container;
 }
@@ -130,6 +101,38 @@ function createDropdowns() {
   });
 
   return container;
+}
+
+function createFilterButton() {
+  const button = document.createElement("button");
+  button.classList.add(
+    "bg-gray-700",
+    "text-white",
+    "rounded-md",
+    "py-2",
+    "px-4",
+    "font-semibold",
+    "flex",
+    "items-center",
+    "gap-2",
+    "self-end",
+  );
+
+  button.innerHTML = `<svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              class="size-3"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2.628 1.601C5.028 1.206 7.49 1 10 1s4.973.206 7.372.601a.75.75 0 0 1 .628.74v2.288a2.25 2.25 0 0 1-.659 1.59l-4.682 4.683a2.25 2.25 0 0 0-.659 1.59v3.037c0 .684-.31 1.33-.844 1.757l-1.937 1.55A.75.75 0 0 1 8 18.25v-5.757a2.25 2.25 0 0 0-.659-1.591L2.659 6.22A2.25 2.25 0 0 1 2 4.629V2.34a.75.75 0 0 1 .628-.74Z"
+                clip-rule="evenodd"
+              />
+            </svg>`;
+  button.innerHTML += "Apply filters";
+
+  return button;
 }
 
 function openModal(choices, button) {
