c7d711501c4c510cb928eb3c2a25774c47b99c47
progchris00 <prog.christian07@gmail.com>
2025-01-27 22:14:56 +0800

    feat: update index js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-import {
-  loadGraphContainer,
-  loadByGenderGraph,
-  loadByPurokGraph,
-} from "./component/Graph/Graph.js";
+import { loadGraphSection } from "./component/Graph/Graph.js";
 import { loadTable } from "./component/Table/Table.js";
 import { loadHeader } from "./component/Header/Header.js";
 
 document.getElementById("view-table-btn").addEventListener("click", () => {
   loadTable();
 });
+
 document.getElementById("view-graph-btn").addEventListener("click", () => {
-  loadGraphContainer();
-  loadByPurokGraph();
-  loadByGenderGraph();
+  loadGraphSection();
 });
 
 loadTable();
