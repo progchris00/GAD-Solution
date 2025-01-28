9e25cb8bb0370ad7ccc2dc262772247aba207640
progchris00 <prog.christian07@gmail.com>
2025-01-28 11:00:48 +0800

    feat: encapsulate button listeners

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import { loadGraphSection } from "./component/Graph/Graph.js";
 import { loadTable } from "./component/Table/Table.js";
 import { loadHeader } from "./component/Header/Header.js";
 
-document.getElementById("view-table-btn").addEventListener("click", () => {
-  loadTable();
-});
-
-document.getElementById("view-graph-btn").addEventListener("click", () => {
-  loadGraphSection();
-});
-
 const loadPage = (function () {
   loadTable();
   loadHeader();
+  attachListener();
 })();
+
+function attachListener() {
+  document.getElementById("view-table-btn").addEventListener("click", () => {
+    loadTable();
+  });
+
+  document.getElementById("view-graph-btn").addEventListener("click", () => {
+    loadGraphSection();
+  });
+}
