58dc6c9a7572c9b256266c3b76086297216bc249
progchris00 <prog.christian07@gmail.com>
2025-02-20 12:41:31 +0800

    feat: edit add button to edit column button

--- src/component/table.js
+++ src/component/table.js
@@ -106,8 +106,8 @@ function createButtons() {
         </svg>`;
   exportImportButton.innerHTML += "Export / Import";
 
-  const addButton = document.createElement("button");
-  addButton.classList.add(
+  const editColumnButton = document.createElement("button");
+  editColumnButton.classList.add(
     "bg-slate-700",
     "py-2",
     "px-4",
@@ -118,21 +118,10 @@ function createButtons() {
     "text-white"
   );
 
-  addButton.innerHTML = `<svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          class="size-4"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
-            clip-rule="evenodd"
-          />
-        </svg>`;
-  addButton.innerHTML += "Add";
+  editColumnButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-4"><path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" /><path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" /></svg>`;
+  editColumnButton.innerHTML += "Edit columns";
 
-  container.append(exportImportButton, addButton);
+  container.append(exportImportButton, editColumnButton);
   return container;
 }
 
