bb7fb1dcf36a661de66c57edd82cbe2c26999885
progchris00 <prog.christian07@gmail.com>
2025-02-20 17:56:14 +0800

    feat: working modal, style need fix

--- src/component/table.js
+++ src/component/table.js
@@ -120,9 +120,45 @@ function createButtons() {
 
   editColumnButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-4"><path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" /><path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" /></svg>`;
   editColumnButton.innerHTML += "Edit columns";
+  editColumnButton.addEventListener("click", () => openEditColumnModal());
 
   container.append(exportImportButton, editColumnButton);
   return container;
+}
+
+async function openEditColumnModal() {
+  const { tableHeading } = await getTableHeading();
+  document.body.innerHTML += `
+    <dialog id="edit-column-modal" class="p-6 w-1/3 shadow-lg">
+      <h1>Edit columns</h1>
+      <div>
+          ${tableHeading
+            .map(
+              (heading) => `
+            <input type="checkbox" id="${heading}" name="columns" value="${heading}" checked><label for="${heading}"> ${heading.name} </label><br>
+            `
+            )
+            .join("")}
+      </div>
+      <div>
+        <button>Cancel</button>
+        <button>Apply</button>
+      </div>
+    </dialog>`;
+  document.getElementById("edit-column-modal").showModal();
+}
+
+async function getTableHeading() {
+  try {
+    const response = await fetch(
+      "http://localhost:9191/api/v1/table-heading/all"
+    );
+    const apiReponse = await response.json();
+
+    return { tableHeading: apiReponse.data };
+  } catch (error) {
+    return { tableHeading: [] };
+  }
 }
 
 const tableSection = (() => {
@@ -159,19 +195,6 @@ const tableSection = (() => {
     table.appendChild(thead);
   }
 
-  async function getTableHeading() {
-    try {
-      const response = await fetch(
-        "http://localhost:9191/api/v1/table-heading/all"
-      );
-      const apiReponse = await response.json();
-
-      return { tableHeading: apiReponse.data };
-    } catch (error) {
-      return { tableHeading: [] };
-    }
-  }
-
   function createTableFooter() {
     const container = document.createElement("div");
     container.classList.add("flex", "justify-between", "mt-2");
@@ -189,8 +212,6 @@ const tableSection = (() => {
     const { data } = await getData();
 
     const tbody = document.createElement("tbody");
-
-    console.log(data[0]);
 
     Object.values(data).forEach((person) => {
       const trElement = document.createElement("tr");
