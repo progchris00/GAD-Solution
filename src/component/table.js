3f4a7809afef811f3f29584935c93ae7d85498b4
progchris00 <prog.christian07@gmail.com>
2025-02-27 22:01:21 +0800

    feat: working edit column

--- src/component/table.js
+++ src/component/table.js
@@ -122,36 +122,45 @@ function createButtons() {
   editColumnButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-4"><path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" /><path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" /></svg>`;
   editColumnButton.innerHTML += "Edit columns";
   editColumnButton.addEventListener("click", () => openEditColumnModal());
+  createEditColumnModal();
 
   container.append(exportImportButton, editColumnButton);
   return container;
 }
 
-async function openEditColumnModal() {
+async function createEditColumnModal() {
   const { tableHeading } = await getTableHeading();
-  document.body.innerHTML += `
-    <dialog id="edit-column-modal" class="p-6 w-1/3 shadow-lg rounded-md">
-      <h2 class="text-xl font-bold mb-4">Edit columns</h1>
-      <div>
-        <ul class="space-y-2">
-          ${tableHeading
-            .map(
-              (heading) => `
-              <li>
-                <label for="${heading.id}" class="flex items-center gap-2">
-                  <input type="checkbox" data-id="${heading.id}" id="${heading.id}" name="columns" value="${heading.name}" ${heading.state === "ACTIVE" ? "checked" : ""}> ${heading.name} 
-                </label>
-              </li>
-            `
-            )
-            .join("")}
-          </ul>
-      </div>
-      <div class="flex gap-4 mt-4 justify-end">
-        <button class="bg-slate-300 text-black rounded-md py-2 px-4 font-semibold" id="cancel-btn">Cancel</button>
-        <button class="bg-slate-700 text-white rounded-md py-2 px-4 font-semibold" id="apply-btn">Apply</button>
-      </div>
-    </dialog>`;
+  const modal = document.createElement("dialog");
+  modal.id = "edit-column-modal";
+  modal.className = "p-6 w-1/3 shadow-lg rounded-md";
+
+  modal.innerHTML = `
+    <h2 class="text-xl font-bold mb-4">Edit columns</h2>
+    <div>
+      <ul class="space-y-2">
+        ${tableHeading
+          .map(
+            (heading) => `
+            <li>
+              <label for="${heading.id}" class="flex items-center gap-2">
+                <input type="checkbox" data-id="${heading.id}" id="${heading.id}" name="columns" value="${heading.name}" ${heading.state === "ACTIVE" ? "checked" : ""}> ${heading.name} 
+              </label>
+            </li>
+          `
+          )
+          .join("")}
+      </ul>
+    </div>
+    <div class="flex gap-4 mt-4 justify-end">
+      <button class="bg-slate-300 text-black rounded-md py-2 px-4 font-semibold" id="cancel-btn">Cancel</button>
+      <button class="bg-slate-700 text-white rounded-md py-2 px-4 font-semibold" id="apply-btn">Apply</button>
+    </div>
+  `;
+
+  document.body.appendChild(modal);
+}
+
+function openEditColumnModal() {
   document.getElementById("edit-column-modal").showModal();
 }
 
